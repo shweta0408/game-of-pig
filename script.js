23b4c2f65d8171741e49a572b4bb2df31c5a2dea
Shweta Mishra <shmis2004@gmail.com>
2022-09-09 18:12:40 +0530

    rolling function

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-console.log("2" + 2 + 5);
-console.log("2" + 2 + 5);
+"use strict";
+
+//Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  //1. Generate Random Dice Roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
