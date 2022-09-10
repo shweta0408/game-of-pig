723a514272533ae047928ee70faf9f9f13ace9cc
Shweta Mishra <shmis2004@gmail.com>
2022-09-10 18:25:10 +0530

    new update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 //Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -16,7 +19,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -31,8 +44,27 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    //current0El.textContent = currentScore; //CHANGE LATER
   } else {
     // switch to next player
+    switchPlayer();
   }
 });
+
+btnHold.addEventListener("click", function () {
+  //1. Add Current score to active player's score
+  scores[activePlayer] += currentScore;
+  //scores[1] = scores[1] + currentScore
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //2. Check if player's score is >= 100
+  if (scores[activePlayer])
+    //Finish the game
+
+    //Switch to the next player
+    switchPlayer();
+});
+console.log("new eddition");
